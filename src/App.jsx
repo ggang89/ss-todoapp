0c5c7c8532bd361e3d6d98078a42e87076da3eb2
ggang89 <ggang89@gmail.com>
2024-09-29 17:46:02 +0900

    inputTodo 컴포넌트 생성하고 props 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-
+import Todoinput from "./components/Todoinput";
+import Todolist from "./components/Todolist";
+import { v4 as uuidv4 } from "uuid";
+import { useState } from "react";
 
 function App() {
-  
+  const [input, setInput] = useState("");
+  const [todoList, setTodoList] = useState([]);
 
+  const addBtn = () => {
+    const newTodo = { id: uuidv4(), todoTitle: input, isEditing: false };
+    setTodoList([newTodo, ...todoList]);
+    setInput("");
+    console.log(newTodo);
+  };
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
   return (
     <>
-     
+      <Todoinput input={input} handleInput={handleInput} addBtn={addBtn} />
+      <Todolist />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Todoinput.jsx
@@ -0,0 +1,16 @@
+export default function Todoinput({ handleInput, addBtn, input }) {
+  return (
+    <>
+      <div>
+        <label id="input">새로운 할 일</label>
+        <input
+          onChange={handleInput}
+          id="input"
+          value={input}
+          placeholder="개발 공부"
+        />
+        <button onClick={addBtn}>추가</button>
+      </div>
+    </>
+  );
+}
